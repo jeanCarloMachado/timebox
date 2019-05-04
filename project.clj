e30aa84c4467f9b52eca0f8a67c3c180dde4599b
JeanCarloMachado <contato@jeancarlomachado.com.br>
2019-05-04 11:49:19 +0200

    use real react clsjs

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  
-  
+
+
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async  "0.4.474"]
-                 [rum "0.11.2"]]
+                 [cljsjs/react-dom "16.8.3-0"]
+                 [cljsjs/react "16.8.3-0"]]
 
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
--- src/timebox/core.cljs
+++ src/timebox/core.cljs
@@ -1,25 +1,10 @@
 (ns timebox.core
-    (:require [rum.core :as rum]))
+    (:require [react-dom]))
 
 (enable-console-print!)
 
 (println "This text is printed from src/timebox/core.cljs. Go ahead and edit it and see reloading in action.")
 
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
-
-(rum/defc hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
-
-(rum/mount (hello-world)
-           (. js/document (getElementById "app")))
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(.render js/ReactDOM
+  (.createElement js/React "h2" nil "Hello, React 22!")
+  (.getElementById js/document "app"))
